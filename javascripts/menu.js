bbf062ae17696e618faa9962e6d8124ec18027f2
smallclover <18363998103@163.com>
2017-09-02 10:48:08 +0800

    日语

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -6,8 +6,8 @@
 
 // $("#menu").html(menu);
 let menu = [
-  '<a id="home" class="item" href="#">Home</a>',
-  '<a id="labmen" class="item" href="#">Lab Mem</a>',
-  '<a id="about" class="item" href="#">About</a>'
+  '<a id="home" class="item" href="#">ホームページ</a>',
+  '<a id="labmen" class="item" href="#">ラボラトリー</a>',
+  '<a id="about" class="item" href="#">について</a>'
 ];
 $("#menu").html(menu.toString().replace(/,/g ,''));
